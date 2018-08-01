a31f6a544df027cf0809dcbb54115c6211b9295f
Soul Bruteflow <Soul-Bruteflow@users.noreply.github.com>
2018-08-01 19:50:13 +0300

    d00ex02 v.1.0

---
+++ d00/ex02/cmake-build-debug/CMakeFiles/d01_e02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/d01_e02.dir/Account.class.cpp.o"
+  "CMakeFiles/d01_e02.dir/tests.cpp.o"
+  "d01_e02.pdb"
+  "d01_e02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/d01_e02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
