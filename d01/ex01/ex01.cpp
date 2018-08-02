dfec3ac3037b19c18b6a60875c5f14fbc5f7bb2d
Soul Bruteflow <Soul-Bruteflow@users.noreply.github.com>
2018-08-02 13:23:49 +0300

    d01 | ex01 | v.1.0

---
+++ d01/ex01/ex01.cpp
@@ -0,0 +1,12 @@
+//
+// Created by bruteflow on 8/2/18.
+//
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
+
